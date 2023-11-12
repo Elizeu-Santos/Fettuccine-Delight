67612bb6390d423ada57465e42bac111c0d161b0
Elizeu Santos <elyzeu.tec@gmail.com>
2023-11-12 19:06:59 -0300

    Add [Reaproveitando header] (#028

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,21 @@ import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styles from './Routes.module.scss';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <header className={styles.header}>
+          <div className={styles.header__text}>Welcome to the Fusion Restaurant Hub</div>
+        </header>
+        <Routes>
+          <Route path='/' element={<Inicio />} />
+          <Route path='cardapio' element={<Cardapio />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
