87f50b015bdd3eccdd39b1b97edf4dddf8414e95
Elizeu Santos <elyzeu.tec@gmail.com>
2023-11-12 19:39:41 -0300

    Add [defaultPage] Página padrão (#029)

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,19 @@
+import DefaultPage from 'components/DefaultPage';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './Routes.module.scss';
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>Welcome to the Fusion Restaurant Hub</div>
-        </header>
         <Routes>
-          <Route path='/' element={<Inicio />} />
-          <Route path='cardapio' element={<Cardapio />} />
+          <Route path='/' element={<DefaultPage />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
         </Routes>
       </Router>
     </main>
