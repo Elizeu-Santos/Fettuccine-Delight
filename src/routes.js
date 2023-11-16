c11c7df5a06b7d2f5a374caf1be41b25597cd107
Elizeu Santos <elyzeu.tec@gmail.com>
2023-11-16 20:22:03 -0300

    Add [NotFound] Adicionar página NotFound

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
+import Footer from 'components/Footer';
 import DefaultPage from 'components/DefaultPage';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
@@ -16,7 +18,9 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
