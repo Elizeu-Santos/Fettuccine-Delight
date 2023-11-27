e1081a6ff1727fef883776024506547491a72b51
Elizeu Santos <elyzeu.tec@gmail.com>
2023-11-27 20:15:40 -0300

    refactor

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,50 +1,42 @@
-import classNames from 'classnames';
 import styles from './Prato.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import DefaultPage from 'components/DefaultPage';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato) {
-    return '';
+  if (!prato) {
+    return <NotFound />;
   }
   return (
-    <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} /> 
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-          <div className={styles.tags}>
-            <div className={classNames({
-              [styles.tags__tipo]: true,
-              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true,
-            })}>
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>
-              {prato.size}g
-            </div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 'S'}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
+    <Routes>
+      <Route path="*" element={<DefaultPage />}>
+        <Route index element={
+          <>
+            <button className={styles.voltar} onClick={() => navigate(-1)}>
+              {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+              <h1 className={styles.titulo}>
+                {prato.title}
+              </h1>
+              <div className={styles.imagem}>
+                <img src={prato.photo} alt={prato.title} />
+              </div>
+              <div className={styles.conteudo}>
+                <p className={styles.conteudo__descricao}>
+                  {prato.description}
+                </p>
+                <TagsPrato {...prato} />
+              </div>
+            </section>
+          </>
+        } />
+      </Route>
+    </Routes>
   );
 }
